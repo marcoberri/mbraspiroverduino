b2d8a9e26cde2694bf8fd22983b3c558a2cbac10
marcoberri <marcoberri@gmail.com>
2014-01-31 14:11:25 +0100

    fix

--- rasprover/app/conf/index.js
+++ rasprover/app/conf/index.js
@@ -7,11 +7,12 @@ config.web = {};
 
 
 //porta della app
-config.web.port = 3001;
+config.web.port = 3001; //test
+//config.web.port = 3000; //prod
 
 //ip app per socket
-config.web.host = 'http://192.168.1.12:' + config.web.port; //prod
-//config.web.host = 'http://localhost:' + config.web.port; //test
+//config.web.host = 'http://192.168.1.12:' + config.web.port; //prod
+config.web.host = 'http://localhost:' + config.web.port; //test
 
 //ip stream server
 config.web.cam = 'http://192.168.1.12:9000';
@@ -25,10 +26,10 @@ config.raspi = {};
 config.raspi.port = '/dev/ttyACM0';
 
 //se true non vengono fatte le chiamate ad arduino
-config.raspi.test = false;
+config.raspi.test = true;
 
 //abilita o meno la ricezione del gyroscopio
-config.raspi.mpu6050 = true;
+config.raspi.mpu6050 = false;
 
 
 /**********************/
